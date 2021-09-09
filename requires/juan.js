d22adfb9c8c3fffe6e7d78d87e58d56f5dafffe7
Sebastian Arias <sebastianrobayog@gmail.com>
2021-09-08 21:21:18 -0500

    Cargando corrección al archivo juan.js

--- requires/juan.js
+++ requires/juan.js
@@ -1,19 +1,21 @@
 const express = require('express')
+const app = express.Router();
 
-const router = express.Router();
+module.exports = app;
 
+//Petición get, muestra en pantalla la instrucción de salida de javascript
 app.get('/JavaScript', function (req, res) {
     console.log("-----------");
     res.send('console.log("Hola Mundo")')  
   }) 
-  
-  app.get('/Java', function (req, res) {
+//Petición get, muestra en la pantalla la instrución de salida de java
+app.get('/Java', function (req, res) {
     console.log("-----------");
     res.send('System.out.println("Hola Mundo")')
     
   }) 
-  
-  app.post('/one', function (req, res) {
+//Se envia el texto (funcion post numero uno)  
+app.post('/one', function (req, res) {
     console.log(req.body);
     res.send('funcion post numero uno')
   });
