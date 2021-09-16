eb8a6e89f8d6ffece7bba5453a901c9264a8c8dd
Sebastian Arias <sebastianrobayog@gmail.com>
2021-09-15 22:08:01 -0500

    Agregando ejercicio typescript (Integer)

---
+++ index2.ts
@@ -0,0 +1,6 @@
+import { suma,resta,multiplicacion,division } from "./requires/juan";
+
+console.log(suma(5,8));
+console.log(resta(5,8));
+console.log(multiplicacion(5,8));
+console.log(division(5,8));
