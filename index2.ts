2b9f68aa14c7362301f1b59d2205cb8150b89840
pablocrdozo <53062091+pablocrdozo@users.noreply.github.com>
2021-09-15 21:59:43 -0500

    Cambios archivo pablo.ts y index2.ts

---
+++ index2.ts
@@ -0,0 +1,44 @@
+import{Var1isDone, Var2isDone, Var3_result_bool, result_bool3, check_A, check_B, check_var_a_and_b, b, f} from "./requires/pablo";
+
+console.log('Tipo de dato boolean (true and false)');
+
+console.log(Var1isDone);
+console.log(Var2isDone);
+console.log('---------------');
+
+
+console.log('&& = Verdadero cuand Var1_bool y Var2_bool son verdaderos');
+
+console.log(Var3_result_bool);
+console.log('---------------');
+
+
+console.log('|| = Verdadero cuando bool1 o bool2 son verdaderos');
+
+console.log(result_bool3);
+console.log('---------------');
+
+
+console.log('!A = verdadero si A es falso.');
+
+console.log(check_A);
+console.log(check_B);
+console.log('---------------');
+
+
+console.log('( !== ) = verdadero cuando A y B son diferentes');
+
+console.log(check_var_a_and_b);
+console.log('---------------');
+
+
+console.log('Caso 1');
+
+console.log(b);
+console.log('---------------');
+
+
+console.log('Caso 2');
+
+console.log(f);
+console.log('---------------');
