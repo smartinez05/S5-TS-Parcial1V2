5d301304ee978f25483b443a398fdfaaca6e99da
many3487 <manuelja@hotmail.es>
2021-09-16 23:52:36 -0500

    mi rama

---
+++ index2.ts
@@ -0,0 +1,14 @@
+import {num1,number1,str1,pal2,pala1,pala2,palab1,bol1,volftt1,bolfft1,bolfff1,carro_color} from './requires/manuel'
+
+console.log(num1);
+console.log(number1);
+console.log(str1);
+console.log(pal2);
+console.log(pala1);
+console.log(pala2);
+console.log(palab1);
+console.log(bol1);
+console.log(volftt1);
+console.log(bolfft1);
+console.log(bolfff1);
+console.log(carro_color);
