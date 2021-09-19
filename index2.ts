80793ba48e046d11cd3eedef7cb858cb3f75e426
many3487 <manuelja@hotmail.es>
2021-09-19 01:42:52 -0500

    arreglos

--- index2.ts
+++ index2.ts
@@ -1,14 +1,7 @@
-import {num1,number1,str1,pal2,pala1,pala2,palab1,bol1,volftt1,bolfft1,bolfff1,carro_color} from './requires/manuel'
+import {frutas, edades, electrodomesticos,estatura,letras} from './requires/manuel';
 
-console.log(num1);
-console.log(number1);
-console.log(str1);
-console.log(pal2);
-console.log(pala1);
-console.log(pala2);
-console.log(palab1);
-console.log(bol1);
-console.log(volftt1);
-console.log(bolfft1);
-console.log(bolfff1);
-console.log(carro_color);
+console.log(frutas);
+console.log(edades);
+console.log(electrodomesticos);
+console.log(estatura);
+console.log(letras);
