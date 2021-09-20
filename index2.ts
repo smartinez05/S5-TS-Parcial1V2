bd9465366e108d971bd5d980d0af63cc8648e6b1
Juan sebastian arias robayo <87985005+SebastianRobayo@users.noreply.github.com>
2021-09-19 21:44:50 -0500

    Update inedex.ts

--- index2.ts
+++ index2.ts
@@ -1,6 +1,21 @@
+
+
+
+
+
+
+
+
+import {frases1,frases2,frases3,frases4} from './requires/sebastian';
 import { suma,resta,multiplicacion,division } from "./requires/juan";
 
+//Sebastian Arias
 console.log(suma(5,8));
 console.log(resta(5,8));
 console.log(multiplicacion(5,8));
 console.log(division(5,8));
+//Sebastian Martinez
+console.log(frases1);
+console.log(frases2);
+console.log(frases3);
+console.log(frases4);
