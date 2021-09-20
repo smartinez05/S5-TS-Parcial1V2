bc0bd903233a1705b6f9615d09e12633191bf9e7
Juan sebastian arias robayo <87985005+SebastianRobayo@users.noreply.github.com>
2021-09-19 21:53:53 -0500

    Update final index2.ts
    Se agregaron todos los archivos finales del trabajo

--- index2.ts
+++ index2.ts
@@ -1,5 +1,28 @@
+import {frases1,frases2,frases3,frases4} from './requires/sebastian';
+import { suma,resta,multiplicacion,division } from "./requires/juan";
+import {frutas, edades, electrodomesticos,estatura,letras} from './requires/manuel';
 import{Var1isDone, Var2isDone, Var3_result_bool, result_bool3, check_A, check_B, check_var_a_and_b, b, f} from "./requires/pablo";
 
+//Sebastian Arias
+console.log(suma(5,8));
+console.log(resta(5,8));
+console.log(multiplicacion(5,8));
+console.log(division(5,8));
+
+//Sebastian Martinez
+console.log(frases1);
+console.log(frases2);
+console.log(frases3);
+console.log(frases4);
+
+//Manuel Quevedo
+console.log(frutas);
+console.log(edades);
+console.log(electrodomesticos);
+console.log(estatura);
+console.log(letras);
+
+//Pablo Melo
 console.log('Tipo de dato boolean (true and false)');
 
 console.log(Var1isDone);
@@ -42,4 +65,3 @@ console.log('Caso 2');
 
 console.log(f);
 console.log('---------------');
-
