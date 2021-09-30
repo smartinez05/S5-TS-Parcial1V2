e82eafb12edd53a3dc777d81266ebab977811737
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2021-09-29 20:37:44 -0500

    limpieza para taller librerias

--- index2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var juan_1 = require("./requires/juan");
-console.log((0, juan_1.suma)(5, 8));
-console.log((0, juan_1.resta)(5, 8));
-console.log((0, juan_1.multiplicacion)(5, 8));
-console.log((0, juan_1.division)(5, 8));
